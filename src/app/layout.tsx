12c73883ae772a3a465c83b6d390c62b75d8afce
priyanshpatel18 <priyansh.180804@gmail.com>
2024-04-06 16:36:56 +0530

    push error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Navbar from "@/components/Navbar"
 import "./globals.css"
 
 export const metadata = {
@@ -12,7 +13,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="bg-[#000515]">
+        <Navbar />
+        {children}
+      </body>
     </html>
   )
 }
