4c350c4428cd45bb2f0e30126670a80171cee6ba
Mr. V <vaidikbhesaniya1212@gmail.com>
2024-04-06 14:05:35 +0530

    Testcommit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
 
 export default function Home(): React.ReactNode {
-  return (
-    <main>
-      <div className="w-full h-screen bg-[#000515]">
-
-      </div>
-    </main>
-  )
+    return (
+        <main>
+            <div className="w-full h-screen bg-[#000515]">
+                <h1 className="text-center text-6xl text-white mt-24">
+                    Bits to Bytes
+                </h1>
+            </div>
+        </main>
+    );
 }
