21f7951dd1ad7b7153c8922737a32cd86d375586
priyanshpatel18 <priyansh.180804@gmail.com>
2024-04-06 14:11:09 +0530

    Conflict Resolved

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 
 export default function Home(): React.ReactNode {
-    return (
-        <main>
-            <div className="w-full h-screen bg-[#000515]">
-                <h1 className="text-center text-6xl text-white mt-24">
-                    Bits to Bytes
-                </h1>
-            </div>
-        </main>
-    );
+  return (
+    <main>
+      <div className="w-full h-screen bg-[#000515]">
+        <h1 className="text-center text-6xl text-white mt-24">
+          Bits to Bytes
+        </h1>
+        <p>oops</p>
+        <h1>Hello</h1>
+      </div>
+    </main>
+  );
 }
